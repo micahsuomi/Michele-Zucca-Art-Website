9604f3a0dd72ae37872096d9dfe1fc0d5dff7330
Michele Zucca <michele.zucca81@gmail.com>
2020-05-08 21:58:33 +0300

    testing deployment

--- src/pages/digital.js
+++ src/pages/digital.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 import {Link, graphql, useStaticQuery } from 'gatsby';
 import portfolioStyles from './portfolio.module.scss';
 import Head from '../components/head';
--- src/pages/playingwiththelightsofsydney.js
+++ src/pages/playingwiththelightsofsydney.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../components/Layout';
+import Layout from '../components/layout';
 import {Link, graphql, useStaticQuery } from 'gatsby';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import portfolioStyles from './portfolio.module.scss';
