163182c047ed3d3e861ffaf172495d07d5bb18b4
Michele Zucca <michele.zucca81@gmail.com>
2020-05-14 14:36:38 +0300

    testing slider for deployment

--- src/components/galleryslider/gallerysliderdata.js
+++ src/components/galleryslider/gallerysliderdata.js
@@ -1,7 +1,7 @@
 import gallery1 from '../../imgs/gallery-1.jpg';
-import gallery2 from '../../imgs/gallery-2.jpg';
+import gallery2 from '../../imgs/gallery-2.png';
 import gallery3 from '../../imgs/gallery-3.jpg';
-import gallery4 from '../../imgs/gallery-4.jpg';
+// import gallery4 from '../../imgs/gallery-4.jpg';
 // import gallery5 from '../../imgs/gallery-5.jpg';
 
 export const galleryData = [
@@ -23,11 +23,11 @@ export const galleryData = [
     description: 'Helsinki Second Batch'
 },
 
-{
+/*{
     img: gallery4,
     link: '/allegories',
     description: 'Allegories'
-},
+},*/
 
 /*{
     img: gallery5,
