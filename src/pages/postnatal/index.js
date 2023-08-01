e4cf56cdd2c58760e7526902c7ea9fdf2bde55e9
Michele Zucca <michele.zucca81@gmail.com>
2023-08-01 22:17:56 +0300

    Added PageTemplate components to postnatal page

---
+++ src/pages/postnatal/index.js
@@ -0,0 +1,44 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+
+import PageTemplate from "../../components/pageTemplate"
+import "../style.scss"
+
+const PostNatal = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      contentfulPostnatalHeader {
+        title
+        description {
+          json
+        }
+      }
+      allContentfulPostnatal {
+        edges {
+          node {
+            title
+            slug
+            image {
+              fluid(maxWidth: 930) {
+                src
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  const { title, description } = data.contentfulPostnatalHeader
+  return (
+    <PageTemplate
+      headTitle="Postnatal"
+      title={title}
+      description={description}
+      contentTypeEdges={data.allContentfulPostnatal.edges}
+      linkUrl="postnatal"
+      styles="vertical"
+    />
+  )
+}
+
+export default PostNatal
