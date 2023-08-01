5971175974d9a5fc4944aa076115d76d91be191a
Michele Zucca <michele.zucca81@gmail.com>
2023-08-01 22:22:45 +0300

    Added PageTemplate to thelordandthenewcreatures page

--- src/pages/thelordandthenewcreatures/index.js
+++ src/pages/thelordandthenewcreatures/index.js
@@ -1,13 +1,7 @@
 import React from "react"
-import { Link, graphql, useStaticQuery } from "gatsby"
-import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
-import Img from "gatsby-image"
+import { graphql, useStaticQuery } from "gatsby"
 
-import Layout from "../../components/layout"
-import Head from "../../components/head"
-
-import portfolioStyles from "../portfolio.module.scss"
-import stylesVertical from "../stylesVertical.module.scss"
+import PageTemplate from "../../components/pageTemplate"
 import "../style.scss"
 
 const TheLordAndTheNewCreatures = () => {
@@ -19,7 +13,7 @@ const TheLordAndTheNewCreatures = () => {
           json
         }
       }
-      allContentfulTheLordAndTheNewCreatures{
+      allContentfulTheLordAndTheNewCreatures {
         edges {
           node {
             title
@@ -36,29 +30,14 @@ const TheLordAndTheNewCreatures = () => {
   `)
   const { title, description } = data.contentfulTheLordAndTheNewCreaturesHeader
   return (
-    <Layout>
-      <Head title="the lord and the new creatures" />
-      <h1>{title}</h1>
-      <p>{documentToReactComponents(description.json)}</p>
-      <ul className={portfolioStyles.wrapper}>
-        {data.allContentfulTheLordAndTheNewCreatures.edges.map(edge => {
-          const { slug, title, image } = edge.node
-          return (
-            <div className={portfolioStyles.card}>
-              <Link
-                to={`/thelordandthenewcreatures/${slug}`}
-                className={portfolioStyles.link}
-              >
-                <h3>{title}</h3>
-                <div class={stylesVertical.imageContainer}>
-                  <Img fluid={image.fluid} src={image.fluid.src} alt={title} />
-                </div>
-              </Link>
-            </div>
-          )
-        })}
-      </ul>
-    </Layout>
+    <PageTemplate
+      headTitle="The lord and the new creatures"
+      title={title}
+      description={description}
+      contentTypeEdges={data.allContentfulTheLordAndTheNewCreatures.edges}
+      linkUrl="thelordandthenewcreatures"
+      styles="vertical"
+    />
   )
 }
 
